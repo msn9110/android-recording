522f779c63c2228390c80c087cd25cbebc7de5c9
HungShiung <msn9110@gmail.com>
2017-08-01 20:21:47 +0800

    Initial commit

---
+++ app/src/main/java/com/danielkim/soundrecorder/FTPManager.java
@@ -0,0 +1,15 @@
+package com.danielkim.soundrecorder;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by HungShiung on 2017/8/1.
+ */
+
+public class FTPManager extends AsyncTask<Void, Void, Void> {
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+}
